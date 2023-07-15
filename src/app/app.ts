45080b5b122a89d7ccc6834523257343fbaf64d4
Chau Tran <nartc7789@gmail.com>
2023-07-15 18:50:31 -0500

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    template: ``,
+    template: `
+        <router-outlet />
+    `,
+    imports: [RouterOutlet],
 })
-export class App {
-    title = 'ng-todomvc-signals';
-}
+export class App {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { App } from './app/app';
+import Todo from './app/todo';
 
-bootstrapApplication(App, { providers: [] }).catch((err) => console.error(err));
+bootstrapApplication(App, {
+    providers: [provideRouter([{ path: '', component: Todo }], withComponentInputBinding())],
+}).catch((err) => console.error(err));
